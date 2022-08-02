98cee9e2ad1c38cc89574234865ba438fb233203
yewon129 <97646746+yewon129@users.noreply.github.com>
2022-08-02 23:30:13 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,14 @@
+def sol(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        return sol(n-1) + sol(n-2) + sol(n-3)
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    print(sol(N))
