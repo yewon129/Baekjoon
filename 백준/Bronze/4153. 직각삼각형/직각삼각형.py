cd32d54ec0126d0d9727fd8d836abe5f464e3beb
yewon129 <97646746+yewon129@users.noreply.github.com>
2022-08-11 22:33:41 +0900

    [Bronze III] Title: 직각삼각형, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,9 @@
+while 1:
+    lst = list(map(int, input().split()))
+    if lst == [0, 0, 0]:
+        break
+    lst.sort()
+    if (lst[0] ** 2 + lst[1] ** 2) == lst[2] ** 2:
+        print('right')
+    else:
+        print('wrong')
