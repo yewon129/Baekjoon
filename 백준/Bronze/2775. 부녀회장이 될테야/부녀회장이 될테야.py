9221d2bb9875561aaf5ee69e1b7f85c8a95b2a7f
yewon129 <97646746+yewon129@users.noreply.github.com>
2022-10-16 18:44:41 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 124 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,10 @@
+T = int(input())
+
+for _ in range(T):
+    k = int(input())
+    n = int(input())
+    floor = [i for i in range(1, n+1)]
+    for i in range(k):
+        for j in range(1, n):
+            floor[j] += floor[j-1]
+    print(floor[-1])
