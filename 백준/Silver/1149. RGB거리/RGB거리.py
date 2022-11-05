fe91aa6b3daaed9c92bf34c2e86296172c0e0036
yewon129 <97646746+yewon129@users.noreply.github.com>
2022-11-05 16:51:15 +0900

    [Silver I] Title: RGB거리, Time: 132 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,9 @@
+N = int(input())
+arr= [list(map(int, input().split())) for _ in range(N) ]
+
+for i in range(1, N):
+    arr[i][0] = min(arr[i-1][1], arr[i-1][2]) + arr[i][0]
+    arr[i][1] = min(arr[i-1][0], arr[i-1][2]) + arr[i][1]
+    arr[i][2] = min(arr[i-1][0], arr[i-1][1]) + arr[i][2]
+
+print(min(arr[N-1][0], arr[N-1][1], arr[N-1][2]))
