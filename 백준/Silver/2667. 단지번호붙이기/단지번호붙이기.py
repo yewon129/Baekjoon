3d13f148efabedcd006322dfe8f756ec5ecd1066
yewon129 <97646746+yewon129@users.noreply.github.com>
2023-03-29 22:34:50 +0900

    [Silver I] Title: 단지번호붙이기, Time: 132 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,32 +1,27 @@
 N = int(input())
+
 arr = [list(map(int, input())) for _ in range(N)]
+visited = [[0] * N for _ in range(N)]
 
-box=[]
 def bfs(sti, stj):
-    global v
+    queue = [(sti, stj)]
+    visited[sti][stj] = 1
     cnt = 1
-    q = [(sti,stj)]
-    v[sti][stj] = 1
-    while q:
-        i, j = q.pop(0)
-        for di,dj in [(0,1), (1,0), (-1,0), (0,-1)]:
+    while queue:
+        i, j = queue.pop(0)
+        for di, dj in [(0,1), (1,0), (0,-1), (-1,0)]:
             ni, nj = i+di, j+dj
-            if 0<=ni<N and 0<=nj<N and v[ni][nj] == 0 and arr[ni][nj] != 0:
-                q.append((ni,nj))
-                v[ni][nj] = v[i][j] + 1
+            if 0<=ni<N and 0<=nj<N and arr[ni][nj] == 1 and visited[ni][nj] == 0:
+                queue.append((ni,nj))
+                visited[ni][nj] = 1
                 cnt += 1
-
-    box.append(cnt)
+    return cnt
+box = []
 
 for i in range(N):
     for j in range(N):
-        if arr[i][j] == 1:
-            v = [[0] * N for _ in range(N)]
-            bfs(i,j)
-            for x in range(N):
-                for y in range(N):
-                    if v[x][y] != 0:
-                        arr[x][y] = 0
+        if arr[i][j] == 1 and visited[i][j] == 0:
+            box.append(bfs(i, j))
 
 box.sort()
 print(len(box))
