0a9cb6ef75372f78656bc337103ceca17c435fa2
yewon129 <97646746+yewon129@users.noreply.github.com>
2023-04-14 16:28:17 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,7 +1,16 @@
 def solution(citations):
     answer = 0
     citations.sort(reverse=True)
-    for i in range(len(citations)):
-        if citations[i] >= i+1:
-            answer = i+1
+    # 6 5 3 1 0
+    for i in range(citations[0], -1, -1):
+        h = 0
+        for j in range(len(citations)):
+            if citations[j] >= i:
+                h += 1
+            else:
+                break
+        if h > i:
+            break
+        if answer < h:
+            answer = h
     return answer
