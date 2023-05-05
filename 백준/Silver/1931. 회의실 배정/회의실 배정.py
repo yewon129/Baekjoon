f15772da3d840f13140d6c8233e2db1f2e12d067
yewon129 <97646746+yewon129@users.noreply.github.com>
2023-05-06 01:19:08 +0900

    [Silver I] Title: 회의실 배정, Time: 2256 ms, Memory: 132240 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,13 +1,9 @@
 N = int(input())
-arr = [0] * N
-for i in range(N):
-    n, m = map(int, input().split())
-    arr[i] = [n, m]
+arr = [list(map(int, input().split())) for _ in range(N)]
 
-arr.sort(key=lambda x:x[0])
-arr.sort(key=lambda x:x[1])
-cnt = 1
+arr.sort(key=lambda x: (x[1], x[0]))
 i = 1
+cnt = 1
 while i < len(arr):
     if arr[i-1][1] <= arr[i][0]:
         cnt += 1
