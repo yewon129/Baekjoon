77c10d5c6de9832a1f5f5367819a86205587366a
yewon129 <97646746+yewon129@users.noreply.github.com>
2023-05-05 20:27:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.75 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,22 +1,23 @@
+from collections import deque
+
 def solution(maps):
     answer = 0
     n = len(maps)
     m = len(maps[0])
-    
-    visited = [[0] * m for _ in range(n)]
-    
-    def bfs(sti,stj):
-        visited[sti][stj] = 1
-        stack = [[sti,stj]]
-        while stack:
-            i, j = stack.pop(0)
+    def bfs():
+        v = [[0]*m for _ in range(n)]
+        v[0][0] = 1
+        q = deque([])
+        q.append([0,0])
+        while q:
+            i, j = q.popleft()
             if i == n-1 and j == m-1:
-                return visited[i][j]
-            for di, dj in [[0, 1], [1,0], [0, -1], [-1,0]]:
-                ni, nj = i+di, j+dj
-                if 0<=ni<n and 0<=nj<m and maps[ni][nj] == 1 and visited[ni][nj] == 0:
-                    visited[ni][nj] = visited[i][j] + 1
-                    stack.append([ni,nj])
+                return v[i][j]
+            for di, dj in [[0,1], [1,0], [0,-1], [-1,0]]:
+                ni , nj = i+di, j+dj
+                if 0<=ni<n and 0<=nj<m and v[ni][nj] == 0 and maps[ni][nj] == 1:
+                    q.append([ni,nj])
+                    v[ni][nj] = v[i][j] + 1
         return -1
-    answer = bfs(0,0)
+    answer = bfs()
     return answer
