afd3ec3b7e6c69b251a1343aef61e7dd39c29292
yewon129 <97646746+yewon129@users.noreply.github.com>
2023-05-05 17:30:39 +0900

    [unrated] Title: 수 조작하기 1, Time: 0.15 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, string control) {
+    int answer = n;
+    for (char c: control){
+        if (c == 'w'){
+            answer += 1;
+        } else if (c == 's'){
+            answer -= 1;
+        } else if (c == 'd'){
+            answer += 10;
+        } else {
+            answer -= 10;
+        }
+    }
+    return answer;
+}
